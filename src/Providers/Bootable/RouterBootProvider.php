ce765831df87532b4aa6f157d9fadbaf25a7cc79
merloxx <hello@merloxx.it>
2023-10-18 00:19:59 +0200

    New: Added RouterBootProvider class

---
+++ src/Providers/Bootable/RouterBootProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\Framework\Providers\Bootable;
+
+use Zaphyr\Config\Contracts\ConfigInterface;
+use Zaphyr\Container\AbstractServiceProvider;
+use Zaphyr\Container\Contracts\BootableServiceProviderInterface;
+use Zaphyr\Router\Contracts\RouterInterface;
+use Zaphyr\Router\Router;
+use Zaphyr\Utils\ClassFinder;
+
+/**
+ * @author merloxx <merloxx@zaphyr.org>
+ */
+class RouterBootProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected array $provides = [
+        RouterInterface::class,
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(): void
+    {
+        $container = $this->getContainer();
+        $config = $container->get(ConfigInterface::class);
+
+        $router = new Router();
+        $router->setContainer($container);
+
+        $controllers = $config->get('routes.controllers', []);
+
+        if (is_string($controllers)) {
+            $controllers = ClassFinder::getClassesFromDirectory($controllers);
+        }
+
+        $router->setControllerRoutes($controllers);
+        $router->setRoutePatterns($config->get('routes.patterns', []));
+        $router->setMiddleware($config->get('routes.middleware', []));
+
+        $container->bindInstance(RouterInterface::class, $router);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(): void
+    {
+        //
+    }
+}
---
+++ tests/Providers/Bootable/RouterBootProviderTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\FrameworkTests\Providers\Bootable;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Zaphyr\Config\Contracts\ConfigInterface;
+use Zaphyr\Container\Contracts\ContainerInterface;
+use Zaphyr\Framework\Providers\Bootable\RouterBootProvider;
+use Zaphyr\Router\Contracts\RouterInterface;
+use Zaphyr\Router\Router;
+
+class RouterBootProviderTest extends TestCase
+{
+    /**
+     * @var ContainerInterface&MockObject
+     */
+    protected ContainerInterface&MockObject $containerMock;
+
+    /**
+     * @var ConfigInterface&MockObject
+     */
+    protected ConfigInterface&MockObject $configMock;
+
+    /**
+     * @var RouterBootProvider
+     */
+    protected RouterBootProvider $routerBootProvider;
+
+    public function setUp(): void
+    {
+        $this->containerMock = $this->createMock(ContainerInterface::class);
+        $this->configMock = $this->createMock(ConfigInterface::class);
+
+        $this->routerBootProvider = new RouterBootProvider();
+        $this->routerBootProvider->setContainer($this->containerMock);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->applicationMock, $this->containerMock, $this->configMock, $this->routerBootProvider);
+    }
+
+    /* -------------------------------------------------
+     * BOOT
+     * -------------------------------------------------
+     */
+
+    public function testBoot(): void
+    {
+        $this->containerMock->expects(self::once())
+            ->method('get')
+            ->with(ConfigInterface::class)
+            ->willReturn($this->configMock);
+
+        $this->configMock->expects(self::exactly(3))
+            ->method('get')
+            ->willReturnCallback(fn ($key) => match ($key) {
+                    'routes.controllers' => [__CLASS__],
+                    'routes.patterns' => ['pattern' => 'value'],
+                    'routes.middleware' => ['Middleware1', 'Middleware2'],
+            });
+
+        $this->containerMock->expects(self::once())
+            ->method('bindInstance')
+            ->with(
+                $this->equalTo(RouterInterface::class),
+                $this->isInstanceOf(Router::class)
+            );
+
+        $this->routerBootProvider->boot();
+    }
+
+    public function testBootWithControllerSrcDirectory(): void
+    {
+        $this->containerMock->expects(self::once())
+            ->method('get')
+            ->with(ConfigInterface::class)
+            ->willReturn($this->configMock);
+
+        $this->configMock->expects(self::exactly(3))
+            ->method('get')
+            ->willReturnCallback(fn ($key) => match ($key) {
+                    'routes.controllers' => __DIR__,
+                    'routes.patterns' => ['pattern' => 'value'],
+                    'routes.middleware' => ['Middleware1', 'Middleware2'],
+            });
+
+        $this->containerMock->expects(self::once())
+            ->method('bindInstance')
+            ->with(
+                $this->equalTo(RouterInterface::class),
+                $this->isInstanceOf(Router::class)
+            );
+
+        $this->routerBootProvider->boot();
+    }
+}
