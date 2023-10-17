babfd69373bb7521b1d01c53d156f80ac2428d5c
merloxx <hello@merloxx.it>
2023-10-17 23:33:43 +0200

    Fixed: Added missing FrameworkException class

---
+++ src/Exceptions/FrameworkException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\Framework\Exceptions;
+
+use Exception;
+
+/**
+ * @author merloxx <merloxx@zaphyr.org>
+ */
+class FrameworkException extends Exception
+{
+}
