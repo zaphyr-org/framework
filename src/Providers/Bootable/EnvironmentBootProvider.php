d501384da8fd59ac8abe8968d3e6f77ed8813c13
merloxx <hello@merloxx.it>
2023-10-18 00:06:29 +0200

    New: Added EnvironmentBootProvider class

---
+++ src/Providers/Bootable/EnvironmentBootProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\Framework\Providers\Bootable;
+
+use Dotenv\Dotenv;
+use Zaphyr\Container\AbstractServiceProvider;
+use Zaphyr\Container\Contracts\BootableServiceProviderInterface;
+use Zaphyr\Framework\Contracts\ApplicationInterface;
+use Zaphyr\Framework\Exceptions\FrameworkException;
+
+/**
+ * @author merloxx <merloxx@zaphyr.org>
+ */
+class EnvironmentBootProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    /**
+     * @param ApplicationInterface $application
+     */
+    public function __construct(protected ApplicationInterface $application)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws FrameworkException if the ".env" file could not be loaded
+     */
+    public function boot(): void
+    {
+        if (file_exists($this->application->getStoragePath('cache' . DIRECTORY_SEPARATOR . 'config.cache'))) {
+            return;
+        }
+
+        $rootPath = $this->application->getRootPath();
+
+        if (!file_exists($rootPath . DIRECTORY_SEPARATOR . '.env')) {
+            throw new FrameworkException('Unable to load the ".env" file');
+        }
+
+        (Dotenv::createImmutable($rootPath))->load();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(): void
+    {
+        //
+    }
+}
---
+++ tests/Providers/Bootable/EnvironmentBootProviderTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\FrameworkTests\Providers\Bootable;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use Zaphyr\Framework\Contracts\ApplicationInterface;
+use Zaphyr\Framework\Exceptions\FrameworkException;
+use Zaphyr\Framework\Providers\Bootable\EnvironmentBootProvider;
+use PHPUnit\Framework\TestCase;
+
+class EnvironmentBootProviderTest extends TestCase
+{
+    /**
+     * @var ApplicationInterface&MockObject
+     */
+    protected ApplicationInterface&MockObject $applicationMock;
+
+    /**
+     * @var EnvironmentBootProvider
+     */
+    protected EnvironmentBootProvider $environmentBootProvider;
+
+    public function setUp(): void
+    {
+        $this->applicationMock = $this->createMock(ApplicationInterface::class);
+
+        $this->environmentBootProvider = new EnvironmentBootProvider($this->applicationMock);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->applicationMock, $this->environmentBootProvider);
+    }
+
+    /* -------------------------------------------------
+     * BOOT
+     * -------------------------------------------------
+     */
+
+    public function testBoot(): void
+    {
+        $this->applicationMock->expects(self::once())
+            ->method('getStoragePath')
+            ->with('cache/config.cache')
+            ->willReturn('');
+
+        $this->applicationMock->expects(self::once())
+            ->method('getRootPath')
+            ->willReturn(dirname(__DIR__, 2) . '/TestAssets/config');
+
+        $this->environmentBootProvider->boot();
+    }
+
+    public function testBootWithConfig(): void
+    {
+        $configCachePath = dirname(__DIR__, 2) . '/TestAssets/config/config.cache';
+
+        file_put_contents($configCachePath, serialize([
+            'app' => [
+                'environment' => 'production',
+            ],
+        ]));
+
+        $this->applicationMock->expects(self::once())
+            ->method('getStoragePath')
+            ->with('cache/config.cache')
+            ->willReturn(dirname(__DIR__, 2) . '/TestAssets/config');
+
+        $this->applicationMock->expects(self::never())
+            ->method('getRootPath');
+
+        $this->environmentBootProvider->boot();
+
+        unlink($configCachePath);
+    }
+
+    public function testBootThrowsExceptionOnMissingEnvFile(): void
+    {
+        $this->expectException(FrameworkException::class);
+
+        $this->applicationMock->expects(self::once())
+            ->method('getStoragePath')
+            ->with('cache/config.cache')
+            ->willReturn('');
+
+        $this->applicationMock->expects(self::once())
+            ->method('getRootPath')
+            ->willReturn(dirname(__DIR__, 2) . '/TestAssets');
+
+        $this->environmentBootProvider->boot();
+    }
+}
