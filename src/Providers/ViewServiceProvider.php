20fa423a3df2b8561f0ef94fd7f664b149ce9b97
merloxx <hello@merloxx.it>
2023-10-20 13:46:37 +0200

    New: Added ViewServiceProvider class

---
+++ src/Providers/ViewServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\Framework\Providers;
+
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+use Zaphyr\Config\Contracts\ConfigInterface;
+use Zaphyr\Container\AbstractServiceProvider;
+use Zaphyr\Framework\Contracts\ApplicationInterface;
+use Zaphyr\Framework\Contracts\View\ViewInterface;
+use Zaphyr\Framework\View\TwigRuntimeLoader;
+use Zaphyr\Framework\View\TwigView;
+
+/**
+ * @author merloxx <merloxx@zaphyr.org>
+ */
+class ViewServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected array $provides = [
+        ViewInterface::class,
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(): void
+    {
+        $this->getContainer()->bindSingleton(ViewInterface::class, fn () => new TwigView($this->getEnvironment()));
+    }
+
+    /**
+     * @return Environment
+     */
+    protected function getEnvironment(): Environment
+    {
+        $container = $this->getContainer();
+        $application = $container->get(ApplicationInterface::class);
+        $config = $container->get(ConfigInterface::class);
+
+        $loader = new FilesystemLoader($application->getResourcesPath('templates', []));
+
+        $environment = new Environment($loader, $config->get('view.options', []));
+        $environment->addRuntimeLoader(new TwigRuntimeLoader($container));
+
+        foreach ($config->get('view.extensions', []) as $extension) {
+            $environment->addExtension($container->get($extension));
+        }
+
+        foreach ($config->get('view.globals', []) as $name => $value) {
+            $environment->addGlobal($name, $value);
+        }
+
+        if ($config->get('app.debug', false)) {
+            $environment->addExtension(new DebugExtension());
+        }
+
+        return $environment;
+    }
+}
---
+++ tests/Integration/Providers/ViewServiceProviderTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Zaphyr\FrameworkTests\Integration\Providers;
+
+use Twig\Extension\DebugExtension;
+use Zaphyr\Config\Contracts\ConfigInterface;
+use Zaphyr\Container\Contracts\ContainerInterface;
+use Zaphyr\Framework\Contracts\View\ViewInterface;
+use Zaphyr\Framework\Providers\ViewServiceProvider;
+use Zaphyr\Framework\View\TwigView;
+use Zaphyr\FrameworkTests\Integration\IntegrationTestCase;
+
+class ViewServiceProviderTest extends IntegrationTestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected ContainerInterface $container;
+
+    /**
+     * @var ViewServiceProvider
+     */
+    protected ViewServiceProvider $viewServiceProvider;
+
+    protected function setUp(): void
+    {
+        static::bootApplication();
+
+        $this->container = static::$application->getContainer();
+
+        $this->viewServiceProvider = new ViewServiceProvider();
+        $this->viewServiceProvider->setContainer($this->container);
+        $this->viewServiceProvider->register();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->container, $this->viewServiceProvider);
+        parent::tearDown();
+    }
+
+    /* -------------------------------------------------
+     * REGISTER
+     * -------------------------------------------------
+     */
+
+    public function testRegister(): void
+    {
+        self::assertTrue($this->viewServiceProvider->provides(ViewInterface::class));
+
+        $config = $this->container->get(ConfigInterface::class);
+        $config->setItems([
+            'view' => [
+                'extensions' => [DebugExtension::class],
+                'globals' => $globals = ['foo' => 'bar'],
+            ],
+        ]);
+
+        /** @var TwigView $view */
+        $view = $this->container->get(ViewInterface::class);
+        $environment = $view->getEnvironment();
+
+        self::assertInstanceOf(TwigView::class, $view);
+        self::assertArrayHasKey(DebugExtension::class, $environment->getExtensions());
+        self::assertEquals($globals, $environment->getGlobals());
+    }
+
+    public function testRegisterWithAppDebugIncludesDebugExtensions(): void
+    {
+        $config = $this->container->get(ConfigInterface::class);
+        $config->setItems([
+            'app' => [
+                'debug' => true,
+            ],
+        ]);
+
+        self::assertArrayHasKey(
+            DebugExtension::class,
+            $this->container->get(ViewInterface::class)->getEnvironment()->getExtensions()
+        );
+    }
+}
