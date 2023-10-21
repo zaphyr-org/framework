9db46da43fa2ec268cfaec3e217d7282d3001748
merloxx <hello@merloxx.it>
2023-10-21 13:37:39 +0200

    New: Added CookieServiceProvider class

---
+++ tests/Integration/Providers/CookieServiceProviderTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaphyr\FrameworkTests\Integration\Providers;
+
+use Zaphyr\Config\Contracts\ConfigInterface;
+use Zaphyr\Container\Contracts\ContainerInterface;
+use Zaphyr\Cookie\Contracts\CookieManagerInterface;
+use Zaphyr\Cookie\Cookie;
+use Zaphyr\Cookie\CookieManager;
+use Zaphyr\Framework\Providers\CookieServiceProvider;
+use Zaphyr\FrameworkTests\Integration\IntegrationTestCase;
+
+class CookieServiceProviderTest extends IntegrationTestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected ContainerInterface $container;
+
+    /**
+     * @var CookieServiceProvider
+     */
+    protected CookieServiceProvider $cookieServiceProvider;
+
+    protected function setUp(): void
+    {
+        $this->container = static::getContainer();
+
+        $this->cookieServiceProvider = new CookieServiceProvider();
+        $this->cookieServiceProvider->setContainer($this->container);
+        $this->cookieServiceProvider->register();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->container, $this->cookieServiceProvider);
+        parent::tearDown();
+    }
+
+    /* -------------------------------------------------
+     * REGISTER
+     * -------------------------------------------------
+     */
+
+    public function testRegister(): void
+    {
+        self::assertTrue($this->cookieServiceProvider->provides(CookieManagerInterface::class));
+
+        $config = $this->container->get(ConfigInterface::class);
+        $config->setItems([
+            'session' => [
+                'expire' => 60,
+                'cookie' => [
+                    'path' => '/',
+                    'domain' => 'example.com',
+                    'secure' => true,
+                    'http_only' => true,
+                    'raw' => false,
+                    'same_site' => Cookie::RESTRICTION_STRICT,
+                ],
+            ],
+        ]);
+
+        $cookieManager = $this->container->get(CookieManagerInterface::class);
+
+        self::assertInstanceOf(CookieManager:: class, $cookieManager);
+    }
+}
